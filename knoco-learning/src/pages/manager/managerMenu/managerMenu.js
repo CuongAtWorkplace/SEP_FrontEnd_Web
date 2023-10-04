60f434b745953cd13b2284cb7d3ff61d8e59bda0
Lê Quang Huy <quanghuy2001pro@gmail.com>
2023-10-04 16:16:17 +0700

    Update manager menu

--- knoco-learning/src/pages/manager/managerMenu/managerMenu.js
+++ knoco-learning/src/pages/manager/managerMenu/managerMenu.js
@@ -2,10 +2,14 @@ import React, { useEffect } from "react";
 import { memo } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChalkboardUser } from '@fortawesome/free-solid-svg-icons';
-import { faChalkboard } from '@fortawesome/free-solid-svg-icons';
-import { faListCheck } from '@fortawesome/free-solid-svg-icons';
-import { faCircleQuestion } from '@fortawesome/free-solid-svg-icons';
+import { faSchool,
+        faCalendarDays,
+        faChalkboardTeacher,
+        faUser,
+        faFlag,
+        faClipboard,
+        faComment,
+        faGear } from '@fortawesome/free-solid-svg-icons';
 import $ from "jquery";
 const ManagerMenu = () => {
     useEffect(() => {
@@ -19,20 +23,36 @@ const ManagerMenu = () => {
         <div className="items">
             <ul>
                 <li>
-                    <FontAwesomeIcon className="menu-icon" icon={faChalkboardUser} />
+                    <FontAwesomeIcon className="menu-icon" icon={faSchool} />
                     <Link className="link-a" to="#">Manage Courses</Link>
                 </li>
                 <li>
-                    <FontAwesomeIcon className="menu-icon" icon={faChalkboard} />
+                    <FontAwesomeIcon className="menu-icon" icon={faCalendarDays} />
                     <Link className="link-a" to="#">View Schedule</Link>
                 </li>
                 <li>
-                    <FontAwesomeIcon className="menu-icon" icon={faListCheck} />
+                    <FontAwesomeIcon className="menu-icon" icon={faChalkboardTeacher} />
                     <Link className="link-a" to="#">Manage Teachers</Link>
                 </li>
                 <li>
-                    <FontAwesomeIcon className="menu-icon" icon={faCircleQuestion} />
+                    <FontAwesomeIcon className="menu-icon" icon={faUser} />
                     <Link className="link-a" to="#">Manage Learners</Link>
+                </li>
+                <li>
+                    <FontAwesomeIcon className="menu-icon" icon={faFlag} />
+                    <Link className="link-a" to="#">View Reports</Link>
+                </li>
+                <li>
+                    <FontAwesomeIcon className="menu-icon" icon={faClipboard} />
+                    <Link className="link-a" to="#">Manage Posts</Link>
+                </li>
+                <li>
+                    <FontAwesomeIcon className="menu-icon" icon={faComment} />
+                    <Link className="link-a" to="#">Manage Comments</Link>
+                </li>
+                <li>
+                    <FontAwesomeIcon className="menu-icon" icon={faGear} />
+                    <Link className="link-a" to="#">Settings</Link>
                 </li>
             </ul>
         </div>

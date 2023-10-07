25da9cfbfd1a893e19bf522610c26a6813effc8c
Lê Quang Huy <quanghuy2001pro@gmail.com>
2023-10-07 21:01:29 +0700

    Add filter and sort table

--- knoco-learning/src/components/Table/Table.js
+++ knoco-learning/src/components/Table/Table.js
@@ -1,7 +1,20 @@
 import React from "react";
-import { useTable } from "react-table";
+import { useTable, useSortBy, useFilters  } from "react-table";
 import './Table.css'
 
+const ColumnFilter = ({ column }) => {
+    const { setFilter } = column;
+  
+    return (
+      <input
+        type="text"
+        onChange={(e) => {
+          setFilter(e.target.value || undefined); // Set undefined to remove the filter entirely
+        }}
+        placeholder={`Filter ${column.Header}`}
+      />
+    );
+  };
 const Table = ({columns, data})=>{
     const{
         getTableProps,
@@ -9,36 +22,53 @@ const Table = ({columns, data})=>{
         headerGroups,
         rows,
         prepareRow,
+        state: { filters, sortBy },
     } = useTable({
         columns, 
         data,
-    })
+        initialState: {
+            sortBy: [{ id: "courseName", desc: false }], // Default sorting column and order
+          },
+        },
+        useFilters, // Use filters hook
+        useSortBy
+        )
     return(
         <div className="board">
-        <table{...getTableProps()}>
+      <table {...getTableProps()}>
         <thead>
-            {headerGroups.map(headerGroups =>(
-                <tr {...headerGroups.getHeaderGroupProps()}>
-                    {headerGroups.headers.map(column => (
-                        <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-                    ))}
-                </tr>
-            ))}
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render("Header")}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? " 🔽"
+                        : " 🔼"
+                      : ""}
+                  </span>
+                  <div>{column.canFilter ? column.render("Filter") : null}</div>
+                </th>
+              ))}
+            </tr>
+          ))}
         </thead>
-        <tbody {...getTableBodyProps}>
-            {rows.map((row,i) => {
-                prepareRow(row)
-                return (
-                    <tr {...row.getRowProps()}>
-                        {row.cells.map(cell => {
-                            return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                        })}
-                    </tr>
-                )
-            })}
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()} key={i}>
+                {row.cells.map((cell) => {
+                  return <td {...cell.getCellProps()} key={cell.column.id}>{cell.render("Cell")}</td>;
+                })}
+              </tr>
+            );
+          })}
         </tbody>
-        </table>
-        </div>
+      </table>
+    </div>
     );
 }
 
--- knoco-learning/src/components/Table/TableListClassTeacher.js
+++ knoco-learning/src/components/Table/TableListClassTeacher.js
@@ -1,18 +1,33 @@
 import React from "react";
 import Table from "./Table";
+const ColumnFilter = ({ column }) => {
+    const { setFilter } = column;
+  
+    return (
+      <input
+        type="text"
+        onChange={(e) => {
+          setFilter(e.target.value || undefined); // Set undefined to remove the filter entirely
+        }}
+        placeholder={`Filter ${column.Header}`}
+      />
+    );
+  };
 
 const TableListClassTeacher = () => {
 
     const columns = [
         {
-            Header: 'Course Name',
-            accessor: 'courseName',
+          Header: 'Course Name',
+          accessor: 'courseName',
+          Filter: ColumnFilter, // Custom filter component for courseName column
         },
         {
-            Header: 'Course ID',
-            accessor: 'courseId',
-        }
-    ];
+          Header: 'Course ID',
+          accessor: 'courseId',
+          Filter: ColumnFilter, // Custom filter component for courseId column
+        },
+      ];
 
     const data = [
         {
@@ -20,11 +35,11 @@ const TableListClassTeacher = () => {
             courseId: "1"
         },
         {
-            courseName: "course 1",
+            courseName: "course 2",
             courseId: "2"
         },
         {
-            courseName: "course 1",
+            courseName: "course 3",
             courseId: "3"
         },
     ];

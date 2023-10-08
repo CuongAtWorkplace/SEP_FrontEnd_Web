5b7fed4cb6bca51eeeccbd65ebc46ce7ec370e27
Nguyen Xuan Ly <nguyenxuanly911@gmail.com>
2023-10-08 15:20:20 +0700

    update Route in App.js

--- knoco-learning/src/App.js
+++ knoco-learning/src/App.js
@@ -6,15 +6,24 @@ import './style/admin/global.scss';
 import Login from './pages/login/Login';
 import Register from './pages/login/Register';
 import ViewAllClass from './pages/teachers/ViewAllClass';
+import Users from './pages/admin/users/Users';
+import Report from './pages/admin/report/Report';
+import User from './pages/admin/user/User';
+import Home from './pages/admin/home/Home';
+
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/vac" element={<ViewAllClass/>} />
-        <Route path="/" element={<Login />}  />
+        {/* <Route path="/" element={<Login />} />
+        <Route path="/vac" element={<ViewAllClass />} />
+        <Route path="/" element={<Login />} /> */}
+        <Route path="/dashborad" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/users/:id" element={<User />} />
+        <Route path="/report" element={<Report />} />
       </Routes>
       <ToastContainer
         position="top-right"

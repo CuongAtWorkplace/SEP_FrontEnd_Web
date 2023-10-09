5cb5381383cd551e8f280861d3d2a1b0bb228d71
Nguyen Xuan Ly <nguyenxuanly911@gmail.com>
2023-10-09 11:33:50 +0700

    Update Users.js

--- knoco-learning/src/pages/admin/users/Users.js
+++ knoco-learning/src/pages/admin/users/Users.js
@@ -1,9 +1,9 @@
 import DataTable from "../../../components/admin/dataTable/DataTable";
 import "./users.scss";
-import React, { Component } from "react";
+import React from "react";
 import { useState , useEffect} from "react";
 import Add from "../../../components/admin/add/Add";
-import { userRows } from "../../../data";
+// import { userRows } from "../../../data";
 
 
 const Users = () => {
@@ -11,35 +11,23 @@ const Users = () => {
   const [userRows, setUserRows] = useState([]); // State để lưu trữ dữ liệu người dùng
 
   useEffect(() => {
-    // Gọi API để lấy dữ liệu người dùng khi thành phần được tạo
-    fetch('https://localhost:7169/api/Admin/GetListUser') // Thay đổi URL thành đường dẫn API thực tế
-      .then((response) => response.json())
-      .then((data) => {
-        // Cập nhật state với dữ liệu người dùng từ phản hồi API
-        setUserRows(data);
-      })
-      .catch((error) => {
-        console.error('Lỗi gọi API:', error);
-      });
+    fetchData();
   }, []);
 
-  return (
-    <div className="users">
-      <div className="info">
-        <h1>Users</h1>
-        <button onClick={() => setOpen(true)}>Add New User</button>
-      </div>
-      <DataTable slug="users" columns={columns} rows={userRows} />
-      {open && <Add slug="user" columns={columns} setOpen={setOpen} />}
-    </div>
-  );
-};
-
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://localhost:7169/api/Admin/GetListUser'); // Thay thế URL bằng API thực tế
+      const data = await response.json();
+      setUserRows(data);
+    } catch (error) {
+      console.error('Lỗi gọi API:', error);
+    }
+  };
 
   const columns = [
-    { field: "id", headerName: "UserId", width: 90 },
+    { field: "userId", headerName: "UserId", width: 90 },
     {
-      field: "img",
+      field: "image",
       headerName: "Image",
       width: 100,
       renderCell: (params) => {
@@ -65,18 +53,31 @@ const Users = () => {
       width: 180,
     },
     {
-      field: "createdAt",
+      field: "address",
       headerName: "Address",
       width: 180,
       type: "string",
     },
     {
-      field: "status",
+      field: "isBan",
       headerName: "Status",
       width: 100,
       type: "string",
     },
   ];
+
+  return (
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button onClick={() => setOpen(true)}>Add New User</button>
+      </div>
+      <DataTable slug="users" columns={columns} rows={userRows} />
+      {open && <Add slug="user" columns={columns} setOpen={setOpen} />}
+    </div>
+  );
+};
+
 
   // const Users = () => {
   //   const [open, setOpen] = useState(false);

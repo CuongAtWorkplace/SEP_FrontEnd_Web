89b76dbd75fa3a79f05a61ee8c046b3875b236c6
nhatvm10 <nhatvuminh00@gmail.com>
2023-10-17 01:02:49 +0700

    detail course

--- knoco-learning/src/App.js
+++ knoco-learning/src/App.js
@@ -14,9 +14,9 @@ import Report from './pages/admin/report/Report';
 import User from './pages/admin/user/User';
 import Home from './pages/admin/home/Home';
 import ViewAllCourse from './pages/manager/Course/ViewAllCourse';
-
+import CourseDetail from './pages/manager/Course/CourseDetail';
 import EditClass from './pages/teachers/EditClass';
-
+import TestModal from './pages/manager/Course/TestModal';
 
 const queryClient = new QueryClient();
 
@@ -35,6 +35,8 @@ function App() {
         <Route path="/users/:id" element={<User />} />
         <Route path="/report" element={<Report />} />
         <Route path="/course" element={<ViewAllCourse />} />
+        <Route path="/testmodal" element={<TestModal />} />
+        <Route path="/coursedetail/:cid" element={<CourseDetail />} />
 
         <Route path="/editclass" element={<EditClass />} />
 

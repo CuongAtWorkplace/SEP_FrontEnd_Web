f3c23df941356791ada52408b360bceb5089c03b
Nguyen Xuan Ly <nguyenxuanly911@gmail.com>
2023-10-19 01:18:22 +0700

    done add user in admin website

--- knoco-learning/src/components/admin/add/Add.js
+++ knoco-learning/src/components/admin/add/Add.js
@@ -1,79 +1,112 @@
 import "./add.scss";
-import { useState } from "react";
+import React, { Component } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-const Add = (props) => {
-  const [fullName, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [address, setAddress] = useState('');
-  const [isBan, setIsBan] = useState('');
+class Add extends Component {
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    // Define the data to be sent to the API based on the form inputs
-    const userAdd = {
-      fullName: fullName,
-      email: email,
-      phone: phone,
-      address: address,
-      status: isBan
+  constructor(props) {
+    super(props);
+    this.state = {
+      // formData: {
+        fullName: '',
+        email: '',
+        phone: '',
+        status: false,
+        address: ''
+      // }
     };
+  }
 
-    // Send a POST request to the API
-    try {
-      const response = await fetch('https://localhost:7169/api/Admin/AddNewUser', {
-        method: "POST",
-        mode: 'cors',
-        credentials: 'same-origin',
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userAdd),
-      });
+  // addNewUser = async (data) => {
+  //   await fetch(`https://localhost:7169/api/Admin/AddNewUser`, {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(data),
+  //   });
+  // }
 
-      if (response.ok) {
-        // Data added successfully, you can handle any success actions here
-        console.log("Data added successfully");
-      } else {
-        // Handle errors if the request was not successful
-        const errorResponse = await response.json();
-        console.error("Error adding data. Server response:", errorResponse);
+  componentDidMount() {
+
+  }
+
+  toggle = () => {
+    this.props.toggleModal();
+  }
+
+  handleOnChangeInput = (event, id) => {
+    let copyState = { ...this.state };
+    copyState[id] = event.target.value;
+
+    this.setState({
+      ...copyState
+    });
+  }
+
+  checkValideInput = () => {
+    let isValid = true;
+    let arrInput = ['fullName', 'email', 'phone', 'status', 'address'];
+    for (let i = 0; i < arrInput.length; i++) {
+      if (!this.state[arrInput[i]]) {
+        isValid = false;
+        alert('Missing parameter:' + arrInput[i]);
+        break;
       }
-
-      // Close the modal
-      props.setOpen(false);
-    } catch (error) {
-      console.error("Error making the API request:", error);
     }
+    return isValid;
+  }
 
-  };
+  handleAddNewUser = () => {
+    let isValid = this.checkValideInput();
+    if (isValid == true) {
+      //call api add user
+      this.props.createNewUser(this.state);
+    }
+  }
 
-  return (
-    <div className="add">
-      <div className="modal">
-        <span className="close" onClick={() => props.setOpen(false)}>
-          X
-        </span>
-        <h1>Add new {props.slug}</h1>
-        <form onSubmit={handleSubmit}>
-          {props.columns
-            .filter((item) => item.field !== "id" && item.field !== "image")
-            .map((column) => (
-              <div className="item" key={column.field}>
-                <label>{column.headerName}</label>
-                <input
-                  type={column.type || "text"} // Add a default type if not provided
-                  name={column.field} // Set the name attribute to match the field name
-                  placeholder={column.field}
-                />
+  render() {
+    return (
+      <Modal isOpen={this.props.isOpen} toggle={() => { this.toggle() }} className={'modal-user'} size="lg" >
+        <ModalHeader style={{ color: 'white', backgroundColor: 'blueviolet' }} toggle={() => { this.toggle() }} >Add new</ModalHeader>
+        <ModalBody>
+          <div className="container">
+            <div className="row">
+              <div className="col-6 form-group">
+                <label>Full Name</label>
+                <input type="text" className="form-control" placeholder="Enter fullname"
+                  onChange={(event) => { this.handleOnChangeInput(event, "fullName") }} value={this.state.fullName} />
               </div>
-            ))}
-          <button type="submit">Send</button>
-        </form>
-      </div>
-    </div>
-  );
+              <div className="col-6 form-group">
+                <label>Email</label>
+                <input type="text" className="form-control" placeholder="Enter email"
+                  onChange={(event) => { this.handleOnChangeInput(event, "email") }} value={this.state.email} />
+              </div>
+              <div className="col-6 form-group">
+                <label>Phone</label>
+                <input type="text" className="form-control" placeholder="Enter phone"
+                  onChange={(event) => { this.handleOnChangeInput(event, "phone") }} value={this.state.phone} />
+              </div>
+              <div className="col-6 form-group">
+                <label>Status</label>
+                <input type="text" className="form-control" placeholder="Enter status"
+                  onChange={(event) => { this.handleOnChangeInput(event, "status") }} value={this.state.status} />
+              </div>
+              <div className="col-12 form-group">
+                <label>Address</label>
+                <input type="text" className="form-control" placeholder="Enter address"
+                  onChange={(event) => { this.handleOnChangeInput(event, "address") }} value={this.state.address} />
+              </div>
+            </div>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={() => { this.handleAddNewUser() }}>Add</Button>{' '}
+          <Button color="secondary" onClick={() => { this.toggle() }}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+    );
+  }
 };
 
 export default Add;
--- knoco-learning/src/pages/admin/users/Users.js
+++ knoco-learning/src/pages/admin/users/Users.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBook } from '@fortawesome/free-solid-svg-icons';
 import DataTable from "../../../components/admin/dataTable/DataTable";
@@ -7,108 +7,162 @@ import Footer from "../../../components/footer/Footer";
 import SideBar from "../../../components/sidebar/SideBar";
 import Add from "../../../components/admin/add/Add";
 import '../users/users.scss';
+import 'bootstrap/dist/css/bootstrap.css';
 
-const Users = () => {
-  const [listUser, setListUser] = useState([]);
-  const [open, setOpen] = useState(false);
-
-  //const [selectedRow, setSelectedRow] = useState(null);
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch('https://localhost:7169/api/Admin/GetListUser');
-      const data = await response.json();
-      setListUser(data.map((user, index) => ({ ...user, id: index }))); // Add unique id
-    } catch (error) {
-      console.error('Lỗi gọi API:', error);
+class Users extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpenModal: false,
+      ListUser: [],
     }
+  }
+
+  getListUser = async () => {
+    try {
+      const response = await fetch(`https://localhost:7169/api/Admin/GetListUser`);
+      const data = await response.json();
+      this.setState({ ListUser: data.map((user, index) => ({ ...user, id: index })) });
+    } catch (error) {
+      console.error('Lỗi gọi API', error);
+    }
+  }
+
+  // addNew = async (data) => {
+  //   const response = await fetch(`https://localhost:7169/api/Admin/AddNewUser`, {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(data),
+  //   });
+  // }
+
+  async componentDidMount() {
+    await this.getListUser();
   };
 
-  const columns = [
-    { field: "userId", headerName: "ID", width: 90 },
-    {
-      field: "image",
-      headerName: "Avatar",
-      width: 100,
-      renderCell: (params) => {
-        return <img src={params.row.image || "/noavatar.png"} alt="" />;
+  handleAddNewUser = () => {
+    this.setState({
+      isOpenModal: true,
+    })
+  }
+
+  toggleModal = () => {
+    this.setState({
+      isOpenModal: !this.state.isOpenModal,
+    })
+  }
+
+  createNewUser = async (data) => {
+    try {
+      const response = await fetch(`https://localhost:7169/api/Admin/AddNewUser`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      if (response.ok) {
+        // alert('User added successfully'); // Thay đổi thông báo
+        await this.getListUser();
+        this.setState({
+          isOpenModal: false
+        });
+      } else {
+        alert('Failed to add user'); // Thay đổi thông báo
+      }
+    } catch (error) {
+      console.error(error);
+      alert('Error occurred'); // Thay đổi thông báo
+    }
+  }
+
+  render() {
+
+    const { ListUser } = this.state;
+    const columns = [
+      { field: "userId", headerName: "ID", width: 90 },
+      {
+        field: "image",
+        headerName: "Avatar",
+        width: 100,
+        renderCell: (params) => {
+          return <img src={params.row.image || "/noavatar.png"} alt="" />;
+        },
       },
-    },
-    {
-      field: "fullName",
-      headerName: "Full Name",
-      width: 150,
-    },
-    {
-      field: "email",
-      headerName: "Email",
-      width: 200,
-    },
-    {
-      field: "phone",
-      headerName: "Phone",
-      width: 180,
-    },
-    {
-      field: "address",
-      headerName: "Address",
-      width: 180,
-    },
-    {
-      field: "isBan",
-      headerName: "Status",
-      width: 100,
-    },
-  ];
+      {
+        field: "fullName",
+        headerName: "Full Name",
+        width: 150,
+      },
+      {
+        field: "email",
+        headerName: "Email",
+        width: 200,
+      },
+      {
+        field: "phone",
+        headerName: "Phone",
+        width: 180,
+      },
+      {
+        field: "address",
+        headerName: "Address",
+        width: 180,
+      },
+      {
+        field: "isBan",
+        headerName: "Status",
+        width: 100,
+      },
+    ];
 
-  // const generateRowId = (row) => row.id;
-
-  // const handleRowClick = (row) => {
-  //   alert(`Clicked on user with ID: ${row.userId}`);
-  //   setSelectedRow(row); // Optionally, you can set the selected row state if needed
-  // };
-
-
-  return (
-    <div>
-      <div className="body_page">
-        <section id="menu">
-          <div className="logo">
-            <FontAwesomeIcon className="logo-icon" icon={faBook} />
-            <h1>Knoco</h1>
-          </div>
-
-          <nav>
-            <SideBar />
-          </nav>
-        </section>
-
-        <section id="interface">
-          <header>
-            <Header />
-          </header>
-          <div className="TableLayout">
-            <div className="users">
-              <div className="info">
-                <h1>Users</h1>
-                <button onClick={() => setOpen(true)}>Add New User</button>
-              </div>
-              <DataTable slug="users" columns={columns} rows={listUser} />
-              {open && <Add slug="user" columns={columns} setOpen={setOpen} />}
+    return (
+      <div>
+        <div className="body_page">
+          <section id="menu">
+            <div className="logo">
+              <FontAwesomeIcon className="logo-icon" icon={faBook} />
+              <h1>Knoco</h1>
             </div>
-          </div>
 
-          <footer>
-            <Footer />
-          </footer>
-        </section>
+            <nav>
+              <SideBar />
+            </nav>
+          </section>
+
+          <section id="interface">
+            <header>
+              <Header />
+            </header>
+            <div className="TableLayout">
+              <div className="users">
+                <div className="info">
+                  <h1>Users</h1>
+                  <div className="mx-1">
+                    <button className="btn btn-primary" onClick={() => { this.handleAddNewUser() }}>
+                      <i className="fas fa-plus"></i>Add New User</button>
+                  </div>
+                  <Add
+                    isOpen={this.state.isOpenModal}
+                    toggleModal={this.toggleModal}
+                    createNewUser={this.createNewUser}
+                  />
+                </div>
+                <DataTable slug="users" columns={columns} rows={ListUser} />
+                {/* {open && <Add slug="user" setOpen={setOpen} />} */}
+              </div>
+            </div>
+
+            <footer>
+              <Footer />
+            </footer>
+          </section>
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
 };
 
 export default Users;

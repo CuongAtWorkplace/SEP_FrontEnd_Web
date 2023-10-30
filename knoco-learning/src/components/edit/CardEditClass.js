cd92a3f252d35b1449b6d1b5d4232f14f633c389
DuctmHE153518 <97203297+DuctmHE153518@users.noreply.github.com>
2023-10-31 00:24:07 +0700

    update class by teacher done

--- knoco-learning/src/components/edit/CardEditClass.js
+++ knoco-learning/src/components/edit/CardEditClass.js
@@ -4,7 +4,15 @@ import './CardEditClass.css'
 
 const CardEditClass = ({ closePopup }) => {
     const params = useParams();
-    const [classDt, setClassDt] = useState(null);
+    const [classDt, setClassDt] = useState({});
+    const [className, setClassName] = useState('');
+    const [topic, setTopic] = useState('');
+    const [fee, setFee] = useState('');
+    const [numberOfWeek, setNumberOfWeek] = useState('');
+    const [numberPhone, setNumberPhone] = useState('');
+    const [description, setDescription] = useState('');
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
 
     useEffect(() => {
         fetchData();
@@ -14,12 +22,15 @@ const CardEditClass = ({ closePopup }) => {
         try {
             const response = await fetch(`https://localhost:7169/api/Class/GetTeacherClassDetail/${params.classId}`); // Thay thế URL bằng API thực tế
             const responseData = await response.json();
-            const formattedClassDt = {
-                ...responseData,
-                startDate: formatDate(responseData.startDate),
-                endDate: formatDate(responseData.endDate)
-            };
-            setClassDt(formattedClassDt);
+            setClassDt(responseData);
+            setClassName(responseData.className || '');
+            setTopic(responseData.topic || '');
+            setFee(responseData.fee || '');
+            setNumberOfWeek(responseData.numberOfWeek || '');
+            setNumberPhone(responseData.numberPhone || '');
+            setDescription(responseData.description || '');
+            setStartDate(formatDate(responseData.startDate) || '');
+            setEndDate(formatDate(responseData.endDate) || '');
         } catch (error) {
             console.error('Lỗi khi lấy dữ liệu lớp học:', error);
         }
@@ -30,38 +41,33 @@ const CardEditClass = ({ closePopup }) => {
         return formattedDate;
     };
 
-    const [formData, setFormData] = useState({
-        ClassName: "${classDt.className}",
-        Topic: "",
-        Fee: "",
-        NumberOfWeek: "",
-        NumberPhone: "",
-        Description: "",
-        StartDate: "",
-        EndDate: "",
-    });
-
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevFormData) => ({
-          ...prevFormData,
-          [name]: value,
-        }));
-      };
-
     const handleSubmit = async (e) => {
+
+        const classUpdate = {
+            classId: classDt.classId,
+            className: className,
+            topic: topic,
+            fee: fee,
+            numberOfWeek: numberOfWeek,
+            numberPhone: numberPhone,
+            description: description
+            //startDate: startDate,
+            //endDate: endDate
+        };
+
         e.preventDefault();
         try {
-            const response = await fetch(`https://localhost:7169/api/Class/EditClass/${params.classId}`, {
+            const response = await fetch(`https://localhost:7169/api/Class/EditClass`, {
                 method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(formData)
+                body: JSON.stringify(classUpdate)
             });
             if (response.ok) {
                 console.log('Dữ liệu lớp học đã được cập nhật thành công');
                 closePopup();
+                window.location.reload();
             } else {
                 console.error('Lỗi khi cập nhật dữ liệu lớp học:', response.status, response.statusText);
             }
@@ -72,40 +78,35 @@ const CardEditClass = ({ closePopup }) => {
 
     return (
         <div className="card-edit-class">
-            {classDt ? (
-                <form onSubmit={handleSubmit}>
-                    <h2>Edit Class</h2>
-                    <label>Class Name:</label>
-                    <input type="text" id="ClassName" name="ClassName" value={formData.className} onChange={handleInputChange} required />
+            <form onSubmit={handleSubmit}>
+                <h2>Edit Class</h2>
+                <label>Class Name:</label>
+                <input type="text" id="ClassName" name="ClassName" value={className} onChange={(e) => setClassName(e.target.value)} required />
 
-                    <label>Topic:</label>
-                    <input type="text" id="Topic" name="Topic" value={formData.topic} onChange={handleInputChange} required />
+                <label>Topic:</label>
+                <input type="text" id="Topic" name="Topic" value={topic} onChange={(e) => setTopic(e.target.value)} required />
 
-                    <label>Fee:</label>
-                    <input type="text" id="Fee" name="Fee" value={formData.fee} onChange={handleInputChange} required />
+                <label>Fee:</label>
+                <input type="text" id="Fee" name="Fee" value={fee} onChange={(e) => setFee(e.target.value)} required />
 
-                    <label>Number of Weeks:</label>
-                    <input type="number" id="NumberOfWeek" name="NumberOfWeek" value={formData.numberOfWeek} onChange={handleInputChange} required />
+                <label>Number of Weeks:</label>
+                <input type="number" id="NumberOfWeek" name="NumberOfWeek" value={numberOfWeek} onChange={(e) => setNumberOfWeek(e.target.value)} required />
 
-                    <label>Phone Number:</label>
-                    <input type="tel" id="NumberPhone" name="NumberPhone" value={formData.numberPhone} onChange={handleInputChange} required />
+                <label>Phone Number:</label>
+                <input type="tel" id="NumberPhone" name="NumberPhone" value={numberPhone} onChange={(e) => setNumberPhone(e.target.value)} required />
 
-                    <label>Description:</label>
-                    <textarea id="Description" name="Description" value={formData.description} onChange={handleInputChange} required />
+                <label>Description:</label>
+                <textarea id="Description" name="Description" value={description} onChange={(e) => setDescription(e.target.value)} required />
 
-                    <label>Start Date:</label>
-                    <input type="date" name="StartDate" value={formData.startDate} onChange={handleInputChange} required />
+                <label>Start Date:</label>
+                <input type="date" name="StartDate" value={startDate} onChange={(e) => setStartDate(e.target.value)} required />
 
-                    <label>End Date:</label>
-                    <input type="date" name="EndDate" value={formData.endDate} onChange={handleInputChange} required />
+                <label>End Date:</label>
+                <input type="date" name="EndDate" value={endDate} onChange={(e) => setEndDate(e.target.value)} required />
 
-                    <input id="submit" type="submit" name="submit" value="Edit" />
-                    <button type="button" onClick={closePopup} className="btn-cancel">Cancel</button>
-                </form>
-            ) : (
-                <p>Loading class information...</p>
-            )}
-
+                <input id="submit" type="submit" name="submit" value="Edit" />
+                <button type="button" onClick={closePopup} className="btn-cancel">Cancel</button>
+            </form>
         </div>
     );
 };

49507577650234a7670f93a6f75a035dfc6b1352
Lê Quang Huy <quanghuy2001pro@gmail.com>
2023-11-01 17:30:51 +0700

    Chat box

--- knoco-learning/src/App.js
+++ knoco-learning/src/App.js
@@ -23,6 +23,7 @@ import QuizzDetail from './pages/teachers/QuizzDetail';
 import VideoCall2 from './pages/teachers/VideoCall2';
 import ViewPostListManager from './pages/manager/ViewPostListManager';
 import ViewPostDetailManager from './pages/manager/ViewPostDetailManager';
+import GroupChat from './pages/teachers/GroupChat';
 //import TestModal from './pages/manager/Course/TestModal';
 
 const queryClient = new QueryClient();
@@ -52,6 +53,7 @@ function App() {
         <Route path="/call" element={<VideoCall2 />} />
         <Route path="/viewpostlistmanager" element={<ViewPostListManager />} />
         <Route path="/viewpostdetailmanager" element={<ViewPostDetailManager />} />
+        <Route path="/chat" element={<GroupChat />} />
       </Routes>
       <ToastContainer
         position="top-right"

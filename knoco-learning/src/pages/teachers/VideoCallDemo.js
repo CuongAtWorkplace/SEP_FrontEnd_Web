3212925066cd4e4e03e6de8030c79a7b423b67c5
nhatvm10 <nhatvuminh00@gmail.com>
2023-11-09 17:35:06 +0700

    commit

--- knoco-learning/src/pages/teachers/VideoCallDemo.js
+++ knoco-learning/src/pages/teachers/VideoCallDemo.js
@@ -1,14 +1,31 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt"
-
+import { useState ,useEffect } from "react";
+import jwtDecode from "jwt-decode";
 const VideoCallDemo = () => {
   const { roomId } = useParams();
+  const [userName1 ,setuserName ] = useState("hiuhiu");
+  const [checkToken , setcheckToken] = useState(false);
+  useEffect(() => {
+    const token = localStorage.getItem("token");  
+    console.log(token);
+    if (token != null) {
+      const decodedToken = jwtDecode(token);
+       setuserName( decodedToken.fullname);
+       setcheckToken(true);
+      //  localStorage.removeItem('token');
+      //   localStorage.removeItem('roleid');
+      //   localStorage.removeItem('id');
+    }else{
+      window.location.href = "/";
+    }
+}, []);
 
   const myMeeting = async (element) => {
     const appID = 699383273;
     const serverSecret = "630bf62f2c2285d34c5a982bc39440f8";
-    const kitToke = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomId, Date.now().toString(), "piii");
+    const kitToke = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomId, Date.now().toString(),userName1);
   const zc = ZegoUIKitPrebuilt.create(kitToke);
   zc.joinRoom({
     container: element,
@@ -22,13 +39,11 @@ const VideoCallDemo = () => {
 });
   }
 
-
-
   return (
     <div>
-      <div class="col-md-6"><div ref={myMeeting}/> </div>
-      <div class="col-md-6">s </div>
+      {checkToken == true && <div class="col-md-6"><div ref={myMeeting}/> </div> }
       
+  
     </div>
   )
 }

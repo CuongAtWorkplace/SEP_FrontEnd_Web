97b6487e131fc06b6cf62b9d68d2da0086723632
nhatvm10 <nhatvuminh00@gmail.com>
2023-11-26 16:31:49 +0700

    phần note

--- knoco-learning/src/components/edit/CardNote.js
+++ knoco-learning/src/components/edit/CardNote.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import myImage from '../../assets/profile.jpg';
 import '../../style/Teacher/Edit.css'
+import jwtDecode from "jwt-decode";
 
 const CardNote = ({ closePopup }) => {
     const UserID = 2;
@@ -10,21 +11,72 @@ const CardNote = ({ closePopup }) => {
     const [selectedImage, setSelectedImage] = useState(null);
     const [imagePreview, setImagePreview] = useState('');
 
+    const params = useParams();
+    const [noteTeacher, setNoteTeacher]= useState([]);
+    const [textAreaValue, setTextAreaValue] = useState('');
+    const [userId , setUserId] = useState('');
+  const handleTextAreaChange = (event) => {
+    setTextAreaValue(event.target.value);
+  };
     useEffect(() => {
-        fetchData();
-        fetchImage();
+        fetchDataNote();
+       
     }, []);
 
-    const fetchData = async () => {
+    const fetchDataNote = async () => {
         try {
-            const response = await fetch(`https://localhost:7169/api/User/GetUserProfile/${UserID}`); // Thay thế URL bằng API thực tế
+            const response = await fetch(`https://localhost:7169/api/NoteTeacher/GetNoteInClass?classId=${params.classId}`); 
             const responseData = await response.json();
-            setUserDt(responseData);
-            setImage(responseData.image || '');
+            setNoteTeacher(responseData);
+           
         } catch (error) {
             console.error('Lỗi khi lấy dữ liệu lớp học:', error);
         }
     };
+    const handleSubmitNote = async (e) => {
+        if(textAreaValue.trim() === ''){
+             alert("không được để trống");
+             return;
+        }else{
+            const token = localStorage.getItem("token");
+            console.log(token);
+           
+              const decodedToken = jwtDecode(token);
+              setUserId(decodedToken.userid);
+         
+          const currentDate = new Date().toISOString();
+            const NoteNew = {
+                userId: decodedToken.userid,
+                classId: params.classId,
+                content: textAreaValue,
+                createDate: currentDate,
+                
+            };
+    
+            e.preventDefault();
+            try {
+                const response = await fetch(`https://localhost:7169/api/NoteTeacher/AddNewNoteTeacher`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(NoteNew)
+                });
+                if (response.ok) {
+                    console.log('Dữ liệu lớp học đã được cập nhật thành công');
+                   fetchDataNote();
+                  setTextAreaValue('');
+                } else {
+                    console.error('Lỗi khi cập nhật dữ liệu lớp học:', response.status, response.statusText);
+                }
+            } catch (error) {
+                console.error('Lỗi khi cập nhật dữ liệu lớp học:', error);
+            }
+        }
+        
+    };
+
+
 
     const fetchImage = async () => {
         try {
@@ -81,12 +133,20 @@ const CardNote = ({ closePopup }) => {
 
     return (
         <div className="card-edit-class">
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmitNote}>
                 <h2>Note</h2>
+               
 
-                <textarea></textarea>
-
-                <button type="submit" id="submit" name="submit" className="btn-btn">Change</button>
+                {noteTeacher.map((note,index)=>(
+                    <div key={index}>
+                        <h3>
+                            {note.createDate} -
+                            {note.content}
+                        </h3>
+                    </div>
+                ))} 
+              <textarea value={textAreaValue} onChange={handleTextAreaChange}></textarea>
+                <button type="submit" id="submit" name="submit" className="btn-btn">Note</button>
                 <button type="button" onClick={closePopup} className="btn-btn">Cancel</button>
             </form>
         </div>
